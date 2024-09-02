ac862a24cd6ee449f09e3a7f0cc6ef03bd0b44db
tnnmigga <tnnmigga@outlook.com>
2024-09-02 12:08:07 +0800

    1

--- game/modules/play/domain/impl/impl.go
+++ game/modules/play/domain/impl/impl.go
@@ -2,14 +2,13 @@ package impl
 
 import (
 	"eastv2/game/modules/play/domain"
+	"eastv2/game/modules/play/domain/impl/event"
 	"eastv2/game/modules/play/domain/impl/msg"
-	"eastv2/game/modules/play/domain/impl/player"
 	"eastv2/game/modules/play/domain/impl/timer"
 )
 
 func Init(d *domain.Domain) {
 	d.PutCase(domain.MsgCaseIndex, msg.New(d))
-	// d.PutCase(domain.EventCaseIndex, event.New(d))
+	d.PutCase(domain.EventCaseIndex, event.New(d))
 	d.PutCase(domain.TimerCaseIndex, timer.New(d))
-	d.PutCase(domain.UserCaseIndex, player.New(d))
 }
